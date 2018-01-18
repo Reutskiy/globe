bba1dc05e18440e68186d8f894f84e011bc2172d
Reutskiy <ra@areutskiy.ru>
2018-01-19 00:04:50 +0300

    w

--- js/earthjs.js
+++ js/earthjs.js
@@ -1066,9 +1066,9 @@ var zoomPlugin = (function () {
         var s0 = __.proj.scale();
         var wh = [__.options.width, __.options.height];
 
-        __.svg.call(d3.zoom().off('zoom start end', zoom).scaleExtent([0.1, 5]).translateExtent([[0, 0], wh]));
+        __.svg.call(d3.zoom().on('zoom start end', zoom).scaleExtent([0.1, 5]).translateExtent([[0, 0], wh]));
 
-        function zoom() {
+        function zoom().{
             var t = d3.event.transform;
             __.proj.scale(s0 * t.k);
             __.resize();
